3d1487cacd1479854499e391148dacc3ae966e3c
MuhDavin213 <davin.new58@gmail.com>
2023-07-05 10:08:04 +0700

    first commit

---
+++ Data_Dokter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace project_akhir
+{
+    public partial class Form_1 : Form
+    {
+        public Form_1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form_1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
--- Data_Pasien.cs
+++ Data_Pasien.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace project_akhir
 {
-    public partial class Form1 : Form
+    public partial class Data_Pasien : Form
     {
-        public Form1()
+        public Data_Pasien()
         {
             InitializeComponent();
         }
