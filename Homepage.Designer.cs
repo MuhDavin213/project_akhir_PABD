8911143648b4b2b79af6c875037ceac0b5399a63
MuhDavin213 <davin.new58@gmail.com>
2023-07-10 19:38:31 +0700

    Merubah Tampilan Homepage

--- Homepage.Designer.cs
+++ Homepage.Designer.cs
@@ -28,12 +28,146 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.DataPerawat = new System.Windows.Forms.Button();
+            this.DataPasien = new System.Windows.Forms.Button();
+            this.DataDokter = new System.Windows.Forms.Button();
+            this.Kamar = new System.Windows.Forms.Button();
+            this.JadwalPraktek = new System.Windows.Forms.Button();
+            this.PiketHarianDokter = new System.Windows.Forms.Button();
+            this.JadwalPerawatPasien = new System.Windows.Forms.Button();
+            this.JadwalPengurusKamar = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label1.Font = new System.Drawing.Font("News706 BT", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(249, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(252, 36);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Rumah Sakit K25";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // DataPerawat
+            // 
+            this.DataPerawat.Location = new System.Drawing.Point(66, 318);
+            this.DataPerawat.Name = "DataPerawat";
+            this.DataPerawat.Size = new System.Drawing.Size(102, 61);
+            this.DataPerawat.TabIndex = 1;
+            this.DataPerawat.Text = "Data Perawat";
+            this.DataPerawat.UseVisualStyleBackColor = true;
+            // 
+            // DataPasien
+            // 
+            this.DataPasien.Location = new System.Drawing.Point(66, 207);
+            this.DataPasien.Name = "DataPasien";
+            this.DataPasien.Size = new System.Drawing.Size(102, 61);
+            this.DataPasien.TabIndex = 2;
+            this.DataPasien.Text = "Data Pasien";
+            this.DataPasien.UseVisualStyleBackColor = true;
+            // 
+            // DataDokter
+            // 
+            this.DataDokter.Location = new System.Drawing.Point(66, 102);
+            this.DataDokter.Name = "DataDokter";
+            this.DataDokter.Size = new System.Drawing.Size(102, 61);
+            this.DataDokter.TabIndex = 3;
+            this.DataDokter.Text = "Data Dokter";
+            this.DataDokter.UseVisualStyleBackColor = true;
+            // 
+            // Kamar
+            // 
+            this.Kamar.Location = new System.Drawing.Point(249, 207);
+            this.Kamar.Name = "Kamar";
+            this.Kamar.Size = new System.Drawing.Size(102, 61);
+            this.Kamar.TabIndex = 4;
+            this.Kamar.Text = "Kamar";
+            this.Kamar.UseVisualStyleBackColor = true;
+            // 
+            // JadwalPraktek
+            // 
+            this.JadwalPraktek.Location = new System.Drawing.Point(416, 207);
+            this.JadwalPraktek.Name = "JadwalPraktek";
+            this.JadwalPraktek.Size = new System.Drawing.Size(102, 61);
+            this.JadwalPraktek.TabIndex = 5;
+            this.JadwalPraktek.Text = "Jadwal Praktek";
+            this.JadwalPraktek.UseVisualStyleBackColor = true;
+            // 
+            // PiketHarianDokter
+            // 
+            this.PiketHarianDokter.Location = new System.Drawing.Point(619, 318);
+            this.PiketHarianDokter.Name = "PiketHarianDokter";
+            this.PiketHarianDokter.Size = new System.Drawing.Size(102, 61);
+            this.PiketHarianDokter.TabIndex = 6;
+            this.PiketHarianDokter.Text = "Piket Harian Dokter";
+            this.PiketHarianDokter.UseVisualStyleBackColor = true;
+            // 
+            // JadwalPerawatPasien
+            // 
+            this.JadwalPerawatPasien.Location = new System.Drawing.Point(619, 207);
+            this.JadwalPerawatPasien.Name = "JadwalPerawatPasien";
+            this.JadwalPerawatPasien.Size = new System.Drawing.Size(102, 61);
+            this.JadwalPerawatPasien.TabIndex = 7;
+            this.JadwalPerawatPasien.Text = "Jadwal Perawat Pasien";
+            this.JadwalPerawatPasien.UseVisualStyleBackColor = true;
+            // 
+            // JadwalPengurusKamar
+            // 
+            this.JadwalPengurusKamar.Location = new System.Drawing.Point(619, 102);
+            this.JadwalPengurusKamar.Name = "JadwalPengurusKamar";
+            this.JadwalPengurusKamar.Size = new System.Drawing.Size(102, 61);
+            this.JadwalPengurusKamar.TabIndex = 8;
+            this.JadwalPengurusKamar.Text = "Jadwal Pengurus Kamar";
+            this.JadwalPengurusKamar.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(87, 16);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Kelompok B4";
+            // 
+            // Homepage
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Green;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.JadwalPengurusKamar);
+            this.Controls.Add(this.JadwalPerawatPasien);
+            this.Controls.Add(this.PiketHarianDokter);
+            this.Controls.Add(this.JadwalPraktek);
+            this.Controls.Add(this.Kamar);
+            this.Controls.Add(this.DataDokter);
+            this.Controls.Add(this.DataPasien);
+            this.Controls.Add(this.DataPerawat);
+            this.Controls.Add(this.label1);
+            this.Name = "Homepage";
             this.Text = "Homepage";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button DataPerawat;
+        private System.Windows.Forms.Button DataPasien;
+        private System.Windows.Forms.Button DataDokter;
+        private System.Windows.Forms.Button Kamar;
+        private System.Windows.Forms.Button JadwalPraktek;
+        private System.Windows.Forms.Button PiketHarianDokter;
+        private System.Windows.Forms.Button JadwalPerawatPasien;
+        private System.Windows.Forms.Button JadwalPengurusKamar;
+        private System.Windows.Forms.Label label2;
     }
 }
