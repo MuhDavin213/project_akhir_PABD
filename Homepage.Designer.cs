291907f335c4516f93db436638ed1a40d9ed9e26
MuhDavin213 <davin.new58@gmail.com>
2023-07-11 20:15:04 +0700

    design homepage

--- Homepage.Designer.cs
+++ Homepage.Designer.cs
@@ -40,9 +40,19 @@
             this.panel1 = new System.Windows.Forms.Panel();
             this.button2 = new System.Windows.Forms.Button();
             this.label2 = new System.Windows.Forms.Label();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.button4 = new System.Windows.Forms.Button();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button5 = new System.Windows.Forms.Button();
+            this.panel6 = new System.Windows.Forms.Panel();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button6 = new System.Windows.Forms.Button();
             this.pnlLeftSide.SuspendLayout();
             this.pnlDataDokter.SuspendLayout();
             this.panel1.SuspendLayout();
+            this.panel5.SuspendLayout();
+            this.panel6.SuspendLayout();
             this.SuspendLayout();
             // 
             // label3
@@ -72,6 +82,8 @@
             // pnlLeftSide
             // 
             this.pnlLeftSide.BackColor = System.Drawing.Color.SkyBlue;
+            this.pnlLeftSide.Controls.Add(this.panel4);
+            this.pnlLeftSide.Controls.Add(this.button4);
             this.pnlLeftSide.Controls.Add(this.panel2);
             this.pnlLeftSide.Controls.Add(this.button1);
             this.pnlLeftSide.Controls.Add(this.panel3);
@@ -106,9 +118,9 @@
             this.button3.Font = new System.Drawing.Font("Sans Serif Collection", 6F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button3.ForeColor = System.Drawing.SystemColors.Control;
             this.button3.Image = ((System.Drawing.Image)(resources.GetObject("button3.Image")));
-            this.button3.Location = new System.Drawing.Point(0, 227);
+            this.button3.Location = new System.Drawing.Point(3, 225);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(216, 66);
+            this.button3.Size = new System.Drawing.Size(213, 66);
             this.button3.TabIndex = 14;
             this.button3.Text = "   Rawat Inap";
             this.button3.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
@@ -125,13 +137,15 @@
             // panel3
             // 
             this.panel3.BackColor = System.Drawing.Color.DeepSkyBlue;
-            this.panel3.Location = new System.Drawing.Point(0, 242);
+            this.panel3.Location = new System.Drawing.Point(0, 240);
             this.panel3.Name = "panel3";
             this.panel3.Size = new System.Drawing.Size(10, 37);
             this.panel3.TabIndex = 20;
             // 
             // pnlDataDokter
             // 
+            this.pnlDataDokter.Controls.Add(this.panel6);
+            this.pnlDataDokter.Controls.Add(this.panel5);
             this.pnlDataDokter.Controls.Add(this.panel1);
             this.pnlDataDokter.Location = new System.Drawing.Point(218, 0);
             this.pnlDataDokter.Name = "pnlDataDokter";
@@ -142,7 +156,7 @@
             // 
             this.panel1.Controls.Add(this.label2);
             this.panel1.Controls.Add(this.button2);
-            this.panel1.Location = new System.Drawing.Point(49, 41);
+            this.panel1.Location = new System.Drawing.Point(36, 41);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(150, 125);
             this.panel1.TabIndex = 0;
@@ -167,6 +181,87 @@
             this.label2.TabIndex = 1;
             this.label2.Text = "Spesialis Mata";
             // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.Color.DeepSkyBlue;
+            this.panel4.Location = new System.Drawing.Point(0, 323);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(10, 37);
+            this.panel4.TabIndex = 22;
+            // 
+            // button4
+            // 
+            this.button4.FlatAppearance.BorderSize = 0;
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button4.Font = new System.Drawing.Font("Sans Serif Collection", 6F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.ForeColor = System.Drawing.SystemColors.Control;
+            this.button4.Image = ((System.Drawing.Image)(resources.GetObject("button4.Image")));
+            this.button4.Location = new System.Drawing.Point(3, 308);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(213, 66);
+            this.button4.TabIndex = 21;
+            this.button4.Text = "   Data Pasien";
+            this.button4.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // panel5
+            // 
+            this.panel5.Controls.Add(this.label4);
+            this.panel5.Controls.Add(this.button5);
+            this.panel5.Location = new System.Drawing.Point(218, 41);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(150, 125);
+            this.panel5.TabIndex = 1;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(29, 93);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(96, 16);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "Spesialis Mata";
+            // 
+            // button5
+            // 
+            this.button5.FlatAppearance.BorderSize = 0;
+            this.button5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button5.Image = ((System.Drawing.Image)(resources.GetObject("button5.Image")));
+            this.button5.Location = new System.Drawing.Point(24, 3);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(93, 78);
+            this.button5.TabIndex = 0;
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // panel6
+            // 
+            this.panel6.Controls.Add(this.label5);
+            this.panel6.Controls.Add(this.button6);
+            this.panel6.Location = new System.Drawing.Point(399, 41);
+            this.panel6.Name = "panel6";
+            this.panel6.Size = new System.Drawing.Size(150, 125);
+            this.panel6.TabIndex = 2;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(29, 93);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(96, 16);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "Spesialis Mata";
+            // 
+            // button6
+            // 
+            this.button6.FlatAppearance.BorderSize = 0;
+            this.button6.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button6.Image = ((System.Drawing.Image)(resources.GetObject("button6.Image")));
+            this.button6.Location = new System.Drawing.Point(24, 3);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(93, 78);
+            this.button6.TabIndex = 0;
+            this.button6.UseVisualStyleBackColor = true;
+            // 
             // Homepage
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
@@ -183,6 +278,10 @@
             this.pnlDataDokter.ResumeLayout(false);
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
+            this.panel5.ResumeLayout(false);
+            this.panel5.PerformLayout();
+            this.panel6.ResumeLayout(false);
+            this.panel6.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -199,6 +298,14 @@
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Panel panel6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button5;
     }
 }
 
