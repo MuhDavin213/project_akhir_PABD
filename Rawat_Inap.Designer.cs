2b2e08732aec229ecda28b77ce517a7a819bee99
MuhDavin213 <davin.new58@gmail.com>
2023-07-12 00:09:37 +0700

    Menambah sedikit UI

--- Rawat_Inap.Designer.cs
+++ Rawat_Inap.Designer.cs
@@ -90,25 +90,27 @@
             // 
             // panel1
             // 
+            this.panel1.BackColor = System.Drawing.Color.Aqua;
             this.panel1.Controls.Add(this.dataGridView1);
             this.panel1.Controls.Add(this.label1);
             this.panel1.Location = new System.Drawing.Point(12, 12);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(419, 170);
+            this.panel1.Size = new System.Drawing.Size(419, 157);
             this.panel1.TabIndex = 0;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(124, 9);
+            this.label1.Font = new System.Drawing.Font("Sitka Display", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(188, 9);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(58, 20);
+            this.label1.Size = new System.Drawing.Size(58, 24);
             this.label1.TabIndex = 0;
             this.label1.Text = "Kamar";
             // 
             // panel2
             // 
+            this.panel2.BackColor = System.Drawing.Color.Aqua;
             this.panel2.Controls.Add(this.dataGridView3);
             this.panel2.Controls.Add(this.label2);
             this.panel2.Location = new System.Drawing.Point(437, 12);
@@ -119,46 +121,51 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(149, 9);
+            this.label2.Font = new System.Drawing.Font("Sitka Display", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(128, 9);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(95, 16);
+            this.label2.Size = new System.Drawing.Size(119, 24);
             this.label2.TabIndex = 1;
             this.label2.Text = "Daftar Perawat";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(115, 9);
+            this.label4.Font = new System.Drawing.Font("Sitka Display", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(81, 9);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(155, 16);
+            this.label4.Size = new System.Drawing.Size(181, 24);
             this.label4.TabIndex = 3;
             this.label4.Text = "Jadwal Pengurus Pasien";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Sitka Display", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.Location = new System.Drawing.Point(73, 9);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(152, 16);
+            this.label3.Size = new System.Drawing.Size(182, 24);
             this.label3.TabIndex = 2;
             this.label3.Text = "Jadwal Pengurus Kamar";
             // 
             // panel3
             // 
+            this.panel3.BackColor = System.Drawing.Color.Cyan;
             this.panel3.Controls.Add(this.dataGridView2);
             this.panel3.Controls.Add(this.label3);
-            this.panel3.Location = new System.Drawing.Point(12, 206);
+            this.panel3.Location = new System.Drawing.Point(9, 223);
             this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(419, 215);
+            this.panel3.Size = new System.Drawing.Size(419, 172);
             this.panel3.TabIndex = 2;
             // 
             // panel4
             // 
+            this.panel4.BackColor = System.Drawing.Color.Cyan;
             this.panel4.Controls.Add(this.dataGridView4);
             this.panel4.Controls.Add(this.label4);
             this.panel4.Location = new System.Drawing.Point(443, 223);
             this.panel4.Name = "panel4";
-            this.panel4.Size = new System.Drawing.Size(345, 215);
+            this.panel4.Size = new System.Drawing.Size(345, 172);
             this.panel4.TabIndex = 3;
             // 
             // dataGridView1
@@ -170,11 +177,11 @@
             this.nokamarDataGridViewTextBoxColumn,
             this.tipeDataGridViewTextBoxColumn});
             this.dataGridView1.DataSource = this.kamarBindingSource;
-            this.dataGridView1.Location = new System.Drawing.Point(3, 32);
+            this.dataGridView1.Location = new System.Drawing.Point(3, 56);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowHeadersWidth = 51;
             this.dataGridView1.RowTemplate.Height = 24;
-            this.dataGridView1.Size = new System.Drawing.Size(413, 129);
+            this.dataGridView1.Size = new System.Drawing.Size(413, 74);
             this.dataGridView1.TabIndex = 1;
             // 
             // puskesmas_superDataSet
@@ -225,11 +232,11 @@
             this.idperawatDataGridViewTextBoxColumn,
             this.idkamarDataGridViewTextBoxColumn1});
             this.dataGridView2.DataSource = this.jadwalPengurusKamarBindingSource;
-            this.dataGridView2.Location = new System.Drawing.Point(3, 28);
+            this.dataGridView2.Location = new System.Drawing.Point(6, 43);
             this.dataGridView2.Name = "dataGridView2";
             this.dataGridView2.RowHeadersWidth = 51;
             this.dataGridView2.RowTemplate.Height = 24;
-            this.dataGridView2.Size = new System.Drawing.Size(398, 150);
+            this.dataGridView2.Size = new System.Drawing.Size(398, 101);
             this.dataGridView2.TabIndex = 3;
             // 
             // puskesmas_superDataSet1
@@ -288,11 +295,11 @@
             this.alamatDataGridViewTextBoxColumn,
             this.noteleponDataGridViewTextBoxColumn});
             this.dataGridView3.DataSource = this.perawatBindingSource;
-            this.dataGridView3.Location = new System.Drawing.Point(4, 28);
+            this.dataGridView3.Location = new System.Drawing.Point(20, 43);
             this.dataGridView3.Name = "dataGridView3";
             this.dataGridView3.RowHeadersWidth = 51;
             this.dataGridView3.RowTemplate.Height = 24;
-            this.dataGridView3.Size = new System.Drawing.Size(326, 150);
+            this.dataGridView3.Size = new System.Drawing.Size(303, 102);
             this.dataGridView3.TabIndex = 2;
             // 
             // puskesmas_superDataSet2
@@ -353,11 +360,11 @@
             this.idperawatDataGridViewTextBoxColumn2,
             this.idpasienDataGridViewTextBoxColumn});
             this.dataGridView4.DataSource = this.jadwalPerawatPasienBindingSource;
-            this.dataGridView4.Location = new System.Drawing.Point(14, 28);
+            this.dataGridView4.Location = new System.Drawing.Point(14, 43);
             this.dataGridView4.Name = "dataGridView4";
             this.dataGridView4.RowHeadersWidth = 51;
             this.dataGridView4.RowTemplate.Height = 24;
-            this.dataGridView4.Size = new System.Drawing.Size(328, 150);
+            this.dataGridView4.Size = new System.Drawing.Size(328, 101);
             this.dataGridView4.TabIndex = 4;
             // 
             // puskesmas_superDataSet3
