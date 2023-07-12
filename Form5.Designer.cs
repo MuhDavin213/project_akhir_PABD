84d9e24c91be86ffbae1b5000510f756942d7734
MuhDavin213 <davin.new58@gmail.com>
2023-07-12 08:10:17 +0700

    add method add pada pengurus kamar dan update

--- Form5.Designer.cs
+++ Form5.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.textBox4 = new System.Windows.Forms.TextBox();
             this.textBox3 = new System.Windows.Forms.TextBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
@@ -42,44 +43,57 @@
             this.button2 = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
             this.button5 = new System.Windows.Forms.Button();
+            this.puskesmas_superDataSet6 = new project_akhir.Puskesmas_superDataSet6();
+            this.jadwalPengurusKamarBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.jadwal_Pengurus_KamarTableAdapter = new project_akhir.Puskesmas_superDataSet6TableAdapters.Jadwal_Pengurus_KamarTableAdapter();
+            this.idjadwalpenguruskamarDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.hariDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.idperawatDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.idkamarDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.puskesmas_superDataSet6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.jadwalPengurusKamarBindingSource)).BeginInit();
             this.SuspendLayout();
             // 
             // textBox4
             // 
-            this.textBox4.Location = new System.Drawing.Point(179, 253);
+            this.textBox4.Location = new System.Drawing.Point(159, 202);
+            this.textBox4.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(294, 26);
+            this.textBox4.Size = new System.Drawing.Size(262, 22);
             this.textBox4.TabIndex = 21;
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(179, 206);
+            this.textBox3.Location = new System.Drawing.Point(159, 165);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(294, 26);
+            this.textBox3.Size = new System.Drawing.Size(262, 22);
             this.textBox3.TabIndex = 20;
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(179, 147);
+            this.textBox2.Location = new System.Drawing.Point(159, 118);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(294, 26);
+            this.textBox2.Size = new System.Drawing.Size(262, 22);
             this.textBox2.TabIndex = 19;
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(179, 99);
+            this.textBox1.Location = new System.Drawing.Point(159, 79);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(294, 26);
+            this.textBox1.Size = new System.Drawing.Size(262, 22);
             this.textBox1.TabIndex = 18;
             // 
             // label4
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(42, 146);
+            this.label4.Location = new System.Drawing.Point(37, 117);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(47, 25);
+            this.label4.Size = new System.Drawing.Size(41, 20);
             this.label4.TabIndex = 17;
             this.label4.Text = "Hari";
             // 
@@ -87,9 +101,9 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(42, 254);
+            this.label3.Location = new System.Drawing.Point(37, 203);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(91, 25);
+            this.label3.Size = new System.Drawing.Size(76, 20);
             this.label3.TabIndex = 16;
             this.label3.Text = "Id Kamar";
             this.label3.Click += new System.EventHandler(this.label3_Click);
@@ -98,9 +112,9 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(42, 98);
+            this.label2.Location = new System.Drawing.Point(37, 78);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(95, 25);
+            this.label2.Size = new System.Drawing.Size(79, 20);
             this.label2.TabIndex = 15;
             this.label2.Text = "Id Jadwal";
             // 
@@ -108,73 +122,134 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(42, 207);
+            this.label1.Location = new System.Drawing.Point(37, 166);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(104, 25);
+            this.label1.Size = new System.Drawing.Size(88, 20);
             this.label1.TabIndex = 14;
             this.label1.Text = "Id Perawat";
             // 
             // dataGridView1
             // 
+            this.dataGridView1.AutoGenerateColumns = false;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(547, 59);
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.idjadwalpenguruskamarDataGridViewTextBoxColumn,
+            this.hariDataGridViewTextBoxColumn,
+            this.idperawatDataGridViewTextBoxColumn,
+            this.idkamarDataGridViewTextBoxColumn});
+            this.dataGridView1.DataSource = this.jadwalPengurusKamarBindingSource;
+            this.dataGridView1.Location = new System.Drawing.Point(486, 47);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowHeadersWidth = 62;
             this.dataGridView1.RowTemplate.Height = 28;
-            this.dataGridView1.Size = new System.Drawing.Size(579, 255);
+            this.dataGridView1.Size = new System.Drawing.Size(515, 204);
             this.dataGridView1.TabIndex = 22;
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(1022, 362);
+            this.button4.Location = new System.Drawing.Point(908, 290);
+            this.button4.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(94, 38);
+            this.button4.Size = new System.Drawing.Size(84, 30);
             this.button4.TabIndex = 26;
             this.button4.Text = "Open";
             this.button4.UseVisualStyleBackColor = true;
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(892, 362);
+            this.button3.Location = new System.Drawing.Point(793, 290);
+            this.button3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(94, 38);
+            this.button3.Size = new System.Drawing.Size(84, 30);
             this.button3.TabIndex = 25;
             this.button3.Text = "Delete";
             this.button3.UseVisualStyleBackColor = true;
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(735, 362);
+            this.button2.Location = new System.Drawing.Point(653, 290);
+            this.button2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(94, 38);
+            this.button2.Size = new System.Drawing.Size(84, 30);
             this.button2.TabIndex = 24;
             this.button2.Text = "Update";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(580, 362);
+            this.button1.Location = new System.Drawing.Point(516, 290);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(94, 38);
+            this.button1.Size = new System.Drawing.Size(84, 30);
             this.button1.TabIndex = 23;
             this.button1.Text = "Add";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button5
             // 
-            this.button5.Location = new System.Drawing.Point(12, 464);
+            this.button5.Location = new System.Drawing.Point(11, 371);
+            this.button5.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(94, 38);
+            this.button5.Size = new System.Drawing.Size(84, 30);
             this.button5.TabIndex = 27;
             this.button5.Text = "Kembali";
             this.button5.UseVisualStyleBackColor = true;
             // 
+            // puskesmas_superDataSet6
+            // 
+            this.puskesmas_superDataSet6.DataSetName = "Puskesmas_superDataSet6";
+            this.puskesmas_superDataSet6.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // jadwalPengurusKamarBindingSource
+            // 
+            this.jadwalPengurusKamarBindingSource.DataMember = "Jadwal_Pengurus_Kamar";
+            this.jadwalPengurusKamarBindingSource.DataSource = this.puskesmas_superDataSet6;
+            // 
+            // jadwal_Pengurus_KamarTableAdapter
+            // 
+            this.jadwal_Pengurus_KamarTableAdapter.ClearBeforeFill = true;
+            // 
+            // idjadwalpenguruskamarDataGridViewTextBoxColumn
+            // 
+            this.idjadwalpenguruskamarDataGridViewTextBoxColumn.DataPropertyName = "id_jadwal_pengurus_kamar";
+            this.idjadwalpenguruskamarDataGridViewTextBoxColumn.HeaderText = "id_jadwal_pengurus_kamar";
+            this.idjadwalpenguruskamarDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.idjadwalpenguruskamarDataGridViewTextBoxColumn.Name = "idjadwalpenguruskamarDataGridViewTextBoxColumn";
+            this.idjadwalpenguruskamarDataGridViewTextBoxColumn.Width = 125;
+            // 
+            // hariDataGridViewTextBoxColumn
+            // 
+            this.hariDataGridViewTextBoxColumn.DataPropertyName = "hari";
+            this.hariDataGridViewTextBoxColumn.HeaderText = "hari";
+            this.hariDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.hariDataGridViewTextBoxColumn.Name = "hariDataGridViewTextBoxColumn";
+            this.hariDataGridViewTextBoxColumn.Width = 125;
+            // 
+            // idperawatDataGridViewTextBoxColumn
+            // 
+            this.idperawatDataGridViewTextBoxColumn.DataPropertyName = "id_perawat";
+            this.idperawatDataGridViewTextBoxColumn.HeaderText = "id_perawat";
+            this.idperawatDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.idperawatDataGridViewTextBoxColumn.Name = "idperawatDataGridViewTextBoxColumn";
+            this.idperawatDataGridViewTextBoxColumn.Width = 125;
+            // 
+            // idkamarDataGridViewTextBoxColumn
+            // 
+            this.idkamarDataGridViewTextBoxColumn.DataPropertyName = "id_kamar";
+            this.idkamarDataGridViewTextBoxColumn.HeaderText = "id_kamar";
+            this.idkamarDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.idkamarDataGridViewTextBoxColumn.Name = "idkamarDataGridViewTextBoxColumn";
+            this.idkamarDataGridViewTextBoxColumn.Width = 125;
+            // 
             // PengurusKamar
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = global::project_akhir.Properties.Resources.pengurus_kamar;
-            this.ClientSize = new System.Drawing.Size(1174, 514);
+            this.ClientSize = new System.Drawing.Size(1044, 411);
             this.Controls.Add(this.button5);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
@@ -189,9 +264,13 @@
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "PengurusKamar";
             this.Text = "Pengurus Kamar";
+            this.Load += new System.EventHandler(this.PengurusKamar_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.puskesmas_superDataSet6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.jadwalPengurusKamarBindingSource)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -213,5 +292,12 @@
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button5;
+        private Puskesmas_superDataSet6 puskesmas_superDataSet6;
+        private System.Windows.Forms.BindingSource jadwalPengurusKamarBindingSource;
+        private Puskesmas_superDataSet6TableAdapters.Jadwal_Pengurus_KamarTableAdapter jadwal_Pengurus_KamarTableAdapter;
+        private System.Windows.Forms.DataGridViewTextBoxColumn idjadwalpenguruskamarDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn hariDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn idperawatDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn idkamarDataGridViewTextBoxColumn;
     }
 }
--- Form5.cs
+++ Form5.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace project_akhir
 {
@@ -20,6 +21,56 @@ namespace project_akhir
         private void label3_Click(object sender, EventArgs e)
         {
 
+        }
+
+        private void PengurusKamar_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'puskesmas_superDataSet6.Jadwal_Pengurus_Kamar' table. You can move, or remove it, as needed.
+            this.jadwal_Pengurus_KamarTableAdapter.Fill(this.puskesmas_superDataSet6.Jadwal_Pengurus_Kamar);
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+                textBox1.Enabled = true;
+                textBox2.Enabled = true;
+                textBox3.Enabled = true;
+                textBox4.Enabled = true;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.SelectedRows.Count == 0)
+            {
+                MessageBox.Show("Pilih Baris yang akan di update", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            string idJadwal = dataGridView1.SelectedRows[0].Cells["id_piket_harian_dokter"].Value.ToString();
+            string hari = textBox1.Text;
+            string idPerawat = textBox2.Text;
+            string idkamar = textBox3.Text;
+
+            if (idJadwal == "")
+            {
+                MessageBox.Show("id piket tidak valid", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+            if (hari == "")
+            {
+                MessageBox.Show("Masukan hari", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+            if (idPerawat == "")
+            {
+                MessageBox.Show("Masukan id perawat", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+            if (idkamar == "")
+            {
+                MessageBox.Show("Masukan id kamar", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
         }
     }
 }
